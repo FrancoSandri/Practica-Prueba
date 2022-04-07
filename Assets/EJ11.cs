5309b9398a4e4964d941f8038e874ed27254f9bc
FrancoSandri <46960579@est.ort.edu.ar>
2022-04-07 18:47:29 -0300

    EJ11 done

---
+++ Assets/EJ11.cs
@@ -0,0 +1,38 @@
+﻿/*11. Realizá un programa que resuelva el siguiente problema:  
+Deberá ingresarse por Inspector una fecha de compra(String en el siguiente formato:  YYYYMMDD), un nombre de comprador, un nombre de producto y una cantidad y precio del producto comprado.Mostrá a modo de ticket, la información ingresada y el monto a pagar.Modelo de Ticket:  
+Fecha de Compra: YYYYMMDD
+Nombre del Comprador: xxxxx xxxxx
+Producto solicitado: xxxxx
+Cantidad solicitada: xx
+Precio Unitario: $xxx
+Total a Pagar: $xxxxx*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EJ11 : MonoBehaviour
+{
+    public string fechaCompra, nombreComprador, nombreProducto;
+    public float cantProducto, precioProducto;
+    float precioTotal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        precioTotal = cantProducto * precioProducto;
+        Debug.Log("Fecha de Compra: " + fechaCompra);
+        Debug.Log("Nombre del Comprador: " + nombreComprador);
+        Debug.Log("Producto solicitado: " + nombreProducto);
+        Debug.Log("Cantidad solicitada: " + cantProducto);
+        Debug.Log("Precio Unitario: $" + precioProducto);
+        Debug.Log("Total a Pagar: $" + precioTotal);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
